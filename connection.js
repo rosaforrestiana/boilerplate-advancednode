2796d15c7f5eedd46c67d62e4e977692a7f46de5
rosaforrestiana <30365715+rosaforrestiana@users.noreply.github.com>
2023-04-07 22:16:58 +0800

    Updated connection.js 7th April 2023

--- connection.js
+++ connection.js
@@ -1,9 +1,10 @@
 // Do not change this file
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
+const URI = process.env['MONGO_URI'];
 
 async function main(callback) {
-    const URI = process.env.MONGO_URI; // Declare MONGO_URI in your .env file
+    const URI = process.env['MONGO_URI']; // Declare MONGO_URI in your .env file
     const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
     try {
