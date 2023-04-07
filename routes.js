dfc6efd44188a1a4ce883f894d5605b3f0b762b3
rosaforrestiana <30365715+rosaforrestiana@users.noreply.github.com>
2023-04-07 22:15:29 +0800

    Updated routes.js 7th April 2023

--- routes.js
+++ routes.js
@@ -1,66 +1,86 @@
 const passport = require('passport');
-const LocalStrategy = require('passport-local');
+const session = require('express-session');
 const bcrypt = require('bcrypt');
-const { ObjectID } = require('mongodb');
-const GitHubStrategy = require('passport-github').Strategy;
-const clientID = process.env['GITHUB_CLIENT_ID'];
-const clientSecret = process.env['GITHUB_CLIENT_SECRET'];
 
-module.exports = function (app, myDataBase) {
-  passport.serializeUser((user, done) => {
-      done(null, user._id);
-  });
-  passport.deserializeUser((id, done) => {
-      myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-          if (err) return console.error(err);
-          done(null, doc);
-      });
-  });
-  passport.use(new LocalStrategy((username, password, done) => {
-    myDataBase.findOne({ username: username }, (err, user) => {
-      console.log(`User ${username} attempted to log in.`);
-      if (err) { return done(err); }
-      if (!user) { return done(null, false); }
-      if (!bcrypt.compareSync(password, user.password)) { 
-          return done(null, false);
-      }
-      return done(null, user);
+module.exports = function(app, myDataBase) {
+  app.route('/').get((req, res) => {
+    res.render('index', {
+      title: 'Connected to Database',
+      message: 'Please log in',
+      showLogin: true,
+      showRegistration: true,
+      showSocialAuth: true
     });
-  }));
- passport.use(new GitHubStrategy({
-      clientID: process.env['GITHUB_CLIENT_ID'],
-      clientSecret: process.env['GITHUB_CLIENT_SECRET'],
-      callbackURL: 'https://boilerplate-advancednode.vlchung.repl.co/auth/github/callback'
-    },
-    function (accessToken, refreshToken, profile, cb) {
-      console.log(profile);
-      //Database logic here with callback containing our user object
-    myDataBase.findOneAndUpdate(
-  { id: profile.id },
-    {
-      $setOnInsert: {
-      id: profile.id,
-      username: profile.username,
-      name: profile.displayName || 'John Doe',
-      photo: profile.photos[0].value || '',
-      email: Array.isArray(profile.emails)
-        ? profile.emails[0].value
-        : 'No public email',
-      created_on: new Date(),
-      provider: profile.provider || ''
-    },
-    $set: {
-      last_login: new Date()
-    },
-    $inc: {
-      login_count: 1
-    }
+  });
+
+  app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/profile');
+  });
+
+  app.route('/profile').get(ensureAuthenticated, (req, res) => {
+    res.render('profile', { username: req.user.username });
+  });
+
+  app.route('/register').post((req, res, next) => {
+    const hash = bcrypt.hashSync(req.body.password, 12);
+    myDataBase.findOne({ username: req.body.username }, (err, user) => {
+      if (err) {
+        next(err);
+      } else if (user) {
+        res.redirect('/');
+      } else {
+        myDataBase.insertOne({
+          username: req.body.username,
+          password: hash
+        },
+          (err, doc) => {
+            if (err) {
+              res.redirect('/');
+            } else {
+              // The inserted document is held within
+              // the ops property of the doc
+              next(null, doc.ops[0]);
+            }
+          }
+        )
+      }
+    })
   },
-  { upsert: true, new: true },
-  (err, doc) => {
-    return cb(null, doc.value);
-       }
-     );   
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      res.redirect('/profile');
     }
-  ));
+  );
+
+  app.route('/auth/github').get(passport.authenticate('github'));
+  app.route('/auth/github/callback').get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+    //res.redirect('/profile');
+    req.session.user_id = req.user.id;
+    res.redirect('/chat');
+  })
+
+  app.route('/chat').get(ensureAuthenticated, (req, res) => {
+    res.render('chat.pug', { user: req.user });
+  });
+
+  app.route('/logout')
+    .get((req, res) => {
+      req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+      });
+    });
+
+  app.use((req, res, next) => {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
+  });
 }
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
+};
